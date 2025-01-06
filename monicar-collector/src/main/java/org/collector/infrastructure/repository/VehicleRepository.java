055764426047e963ca84588b57a3788d80fefcb9
Suxxxxhyun <qkrtngus116@naver.com>
2025-01-06 10:19:52 +0900

    refactor: 도메인명 Vehicle -> VehicleInformation으로 맞추기

--- monicar-collector/src/main/java/org/collector/infrastructure/repository/VehicleRepository.java
+++ monicar-collector/src/main/java/org/collector/infrastructure/repository/VehicleRepository.java
@@ -2,9 +2,9 @@ package org.collector.infrastructure.repository;
 
 import java.util.Optional;
 
-import org.collector.domain.Vehicle;
+import org.collector.domain.VehicleInformation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
-	Optional<Vehicle> findByMdn(Long mdn);
+public interface VehicleRepository extends JpaRepository<VehicleInformation, Long> {
+	Optional<VehicleInformation> findByMdn(Long mdn);
 }
